88e1e98d3de124d87acaa7bf0f098075f6bf2f36
Your Name <brandon.sellam@gmail.com>
2020-06-12 19:04:36 +0200

    redo the stuff

---
+++ README.md
@@ -0,0 +1,21 @@
+
+### Markdown
+
+open  the app in the terminal /
+node server
+/ deploye that link on your chrome then have fun 
+http://localhost:8080/notes
+
+you use this link for the app
+https://note6bs.herokuapp.com/
+
+### Contact me 
+
+
+
+![ProfileImage](https://media.giphy.com/media/SXgbKiEY7lHrr3vvz8/giphy.gif)
+<iframe src="https://giphy.com/embed/jSKbVTyuwGEl866mfD" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/jSKbVTyuwGEl866mfD">via GIPHY</a></p>
+
+brandon.sellam@gmail.com
+
+
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const port = 8080;
+const mainDir = path.join(__dirname, "/public");
+var PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//api to get notes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
+//api to get notes
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+//api to get particular note
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+});
+//api to save particular note
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content: ", newNote);
+    res.json(savedNotes);
+})
+
+//api to delete particular note
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
+app.listen(PORT, function() {
+    console.log(`Now listening to port ${PORT}. Enjoy your stay!`);
+})
